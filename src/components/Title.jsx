26b08df2db95f6d43900d71cd18e2982f716116f
Danijela2019 <arsic_danijela@yahoo.com>
2020-10-04 10:54:33 +0200

    Changed the favicon

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,7 +4,7 @@ import Container from "react-bootstrap/Container";
 
 const Title = () => {
   return (
-    <Jumbotron className="pb-1 text-center " fluid>
+    <Jumbotron className="pb-1 m-2 text-center " fluid>
       <Container>
         <h1>Welcome to Git-net</h1>
         <p>...a place to connect with other like-minded people... </p>
