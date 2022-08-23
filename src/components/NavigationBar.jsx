87ffc4ad8d03d738bfe11591ea8ba4903e7df728
danijela.milenkovic <danijela.milenkovic@ddsthlm.se>
2022-08-23 22:24:10 +0200

    Fix some margin issues

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,28 +1,34 @@
 import React from "react";
-import { Navbar, Button, Form, FormControl } from "react-bootstrap";
+import { Navbar, Button, Form, FormControl, Container } from "react-bootstrap";
 import { GoMarkGithub } from "react-icons/go";
 
 import IconPlacer from "./IconPlacer";
 
 const NavigationBar = ({ submit, value, change }) => {
   return (
-    <Navbar bg="info" variant="dark" className="font-weight-bold">
+    <Navbar
+      bg="info"
+      variant="dark"
+      className="font-weight-bold d-flex flex-column flex-md-row"
+    >
       <IconPlacer>
         <GoMarkGithub />
       </IconPlacer>
       <Navbar.Brand>GIT-NET</Navbar.Brand>
-      <Form inline onSubmit={submit}>
-        <FormControl
-          type="text"
-          placeholder="Github username"
-          value={value}
-          className="mr-sm-2"
-          onChange={change}
-        />
-        <Button variant="outline-light" type="submit">
-          Search
-        </Button>
-      </Form>
+      <Container className="w-100">
+        <Form className=" d-flex w-100" onSubmit={submit}>
+          <FormControl
+            type="text"
+            placeholder="Github username"
+            value={value}
+            className="mr-2"
+            onChange={change}
+          />
+          <Button variant="outline-light" type="submit">
+            Search
+          </Button>
+        </Form>
+      </Container>
     </Navbar>
   );
 };
